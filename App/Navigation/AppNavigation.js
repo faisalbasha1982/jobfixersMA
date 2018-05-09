1312142accda61aa0466ec097b4e05126b1b3a08
Jobfixers <jobfixers@Jobfixerss-iMac.local>
2018-05-09 16:57:53 +0400

    first commit

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,22 @@
 import { StackNavigator } from 'react-navigation'
+import PizzaLocationListScreen from '../Containers/PizzaLocationListScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
+import LanguageScreen from '../Containers/LanguageScreen'
+import NewScreen from '../Containers/NewScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+  PizzaLocationListScreen: { screen: PizzaLocationListScreen },
+  LaunchScreen: { screen: LaunchScreen },
+  LanguageScreen: { screen: LanguageScreen },
+  NewScreen: { screen: NewScreen }
+
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'NewScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
